27a5e9b4a7f5352023ed0cb8207d9ced1c9f229c
NAHIN-JZS <syednahinhossain@gmail.com>
2023-03-29 01:41:20 +0600

    lowest count photo come first

--- submit_review.php
+++ submit_review.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $update_review_sum = $review_sum + $rating;
     $update_count = $count + 1;
     $update_avg_review = $update_review_sum / $update_count;
-    echo 'Thank you for submitting your review. Your rating is: ' . $rating."  ". $update_avg_review . "    " . $update_count . " ". $update_review_sum;
+    // echo 'Thank you for submitting your review. Your rating is: ' . $rating."  ". $update_avg_review . "    " . $update_count . " ". $update_review_sum;
     
     $sql_update_rating = "UPDATE `image_info` SET `review_sum` = '$update_review_sum', `count`='$update_count', `avg_review`='$update_avg_review' WHERE `image_info`.`id` = '$image_id';";
     mysqli_query($connect, $sql_update_rating);
@@ -27,10 +27,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     // $_POST['image_path'] = "images/running-free-m.jpg";
 
-    $new_image_path = "images/running-free-m.jpg";
-    $new_image_id = "45";
+    $get_one_image_with_lowest_count = 'SELECT * FROM `image_info` WHERE `image_info`. `count` = (SELECT MIN(`image_info`. `count`) FROM `image_info`) LIMIT 1;';
+    
+    $new_data = mysqli_query($connect, $get_one_image_with_lowest_count);
+
+	foreach ($new_data as $key => $value) {
+		$new_image_id = $value['id'];
+        $new_image_path = $value['path'];
+		// $review_sum = $value['review_sum'];
+		// $count = $value['count'];
+		// $avg_review = $value['avg_review'];
+	}
+
+    // $new_image_path = "images/running-free-m.jpg";
+    // $new_image_id = "36";
     $url = "index.php?image_path=" . urlencode($new_image_path) . "&new_image_id=" . urlencode($new_image_id);
-    // header("Location: " . $url);
+    header("Location: " . $url);
     // echo $_POST['image_path'];
     // echo $url;
 
