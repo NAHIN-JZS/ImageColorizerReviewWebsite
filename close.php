d2d0b8f3ec5401c70cc8247e8d53252d12083334
NAHIN-JZS <syednahinhossain@gmail.com>
2023-04-09 14:06:17 +0600

    exit page modified

--- close.php
+++ close.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html>
     <body>
-        <p> Thank you for your help. This tab will automatically close in 10 seconds.</p>
+        <!-- <p> Thank you for your help. This tab will automatically close in 10 seconds.</p> -->
+        <div style = "width: 100%; max-width: 1000px;  margin: 0 auto; padding: 0 55px; text-align: center;">
+                                    <h2>Thank you for your help.</h2>
+                                    <p class="opaque-black">This tab will automatically close in 10 seconds.
+                                    </p>
+                </div>
         
         <script>
 		setTimeout(function() {
